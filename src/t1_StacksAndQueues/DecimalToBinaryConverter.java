c3c45bcbec17eb8459f76b2c180c9fa3ce75eefe
barishm <barishm@outlook.com>
2022-05-21 19:55:07 +0200

    exercise

---
+++ src/t1_StacksAndQueues/DecimalToBinaryConverter.java
@@ -0,0 +1,22 @@
+package t1_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinaryConverter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int decimal = Integer.valueOf(scan.nextLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        while (decimal != 0){
+            stack.push(decimal % 2);
+            decimal /= 2;
+
+        }
+        while (!stack.isEmpty()){
+            System.out.print(stack.pop());
+        }
+
+    }
+}
