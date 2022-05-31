1be1b39d433d8c9795205a59408c960c9c75796f
barishm <barishm@outlook.com>
2022-05-31 22:35:24 +0200

    exercise

--- src/t1_StacksAndQueues/BalancedParentheses.java
+++ src/t1_StacksAndQueues/BalancedParentheses.java
@@ -7,24 +7,35 @@ import java.util.Scanner;
 public class BalancedParentheses {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        boolean YES = true;
+        boolean isBalanced = true;
 
-        Deque<Character> left = new ArrayDeque<>();
-        Deque<Character> right = new ArrayDeque<>();
+        Deque<Character> openedBracket = new ArrayDeque<>();
+
         String expression = scan.nextLine();
         for (int i = 0; i < expression.length(); i++) {
-            char ch = expression.charAt(i);
-            if(ch == '{' | ch == '(' | ch == '['){
-                left.push(ch);
-            }else if(ch == '}' | ch == ')' | ch == ']'){
-                right.push(ch);
+            char currentBracket = expression.charAt(i);
+            if (currentBracket == '{' || currentBracket == '(' || currentBracket == '[') {
+                openedBracket.push(currentBracket);
+            } else {
+                if (openedBracket.isEmpty()) {
+                    isBalanced = false;
+                    break;
+                }
+                char lastBracket = openedBracket.pop();
+                if (currentBracket == '}' && lastBracket != '{') {
+                    isBalanced = false;
+                } else if (currentBracket == ')' && lastBracket != '(') {
+                    isBalanced = false;
+                } else if (currentBracket == ']' && lastBracket != '[') {
+                    isBalanced = false;
+                }
             }
         }
-        for (int i = 0; i < left.size(); i++) {
-            if(left.pop() == right.pop()){
 
-            }
-
+        if (isBalanced) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
     }
 }
