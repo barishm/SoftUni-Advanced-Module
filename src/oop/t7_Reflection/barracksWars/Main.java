e5449eecad55714b1f15b7f5d4bbab28676cc143
barishm <barishm@outlook.com>
2022-08-13 12:07:52 +0200

    exercise

--- src/oop/t7_Reflection/barracksWars/Main.java
+++ src/oop/t7_Reflection/barracksWars/Main.java
@@ -1,19 +1,23 @@
 package oop.t7_Reflection.barracksWars;
 
-import oop.t7_Reflection.barracksWars.interfaces.Repository;
-import oop.t7_Reflection.barracksWars.interfaces.Runnable;
-import oop.t7_Reflection.barracksWars.interfaces.UnitFactory;
+import oop.t7_Reflection.barracksWars.core.CommandInterpreterImpl;
 import oop.t7_Reflection.barracksWars.core.Engine;
 import oop.t7_Reflection.barracksWars.core.factories.UnitFactoryImpl;
 import oop.t7_Reflection.barracksWars.data.UnitRepository;
+import oop.t7_Reflection.barracksWars.interfaces.Repository;
+import oop.t7_Reflection.barracksWars.interfaces.UnitFactory;
 
 public class Main {
-
     public static void main(String[] args) {
-        Repository repository = new UnitRepository();
-        UnitFactory unitFactory = new UnitFactoryImpl();
 
-        Runnable engine = new Engine(repository, unitFactory);
+        final Repository repository = new UnitRepository();
+
+        final UnitFactory unitFactory = new UnitFactoryImpl();
+
+        final CommandInterpreterImpl commandInterpreter = new CommandInterpreterImpl(repository, unitFactory);
+
+        final Engine engine = new Engine(commandInterpreter);
+
         engine.run();
     }
 }
