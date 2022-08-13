2a73a96a238beb483d05d70e6290e0a21bc572c7
barishm <barishm@outlook.com>
2022-08-13 11:27:07 +0200

    exercise

--- src/oop/t7_Reflection/blackBoxInteger/Main.java
+++ src/oop/t7_Reflection/blackBoxInteger/Main.java
@@ -1,7 +1,42 @@
 package oop.t7_Reflection.blackBoxInteger;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
+    private final static Scanner SCANNER = new Scanner(System.in);
+    private final static Map<String, Method> NAME_METHOD_MAP =
+            Arrays.stream(BlackBoxInt.class.getDeclaredMethods())
+                    .peek(method -> method.setAccessible(true))
+                    .collect(Collectors.toMap(Method::getName,method -> method));
+    private final static String END_COMMAND = "END";
+    private  final static String RESULT = "innerValue";
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        final Constructor<BlackBoxInt> blackBoxIntConstructor =
+                BlackBoxInt.class.getDeclaredConstructor(int.class);
+        blackBoxIntConstructor.setAccessible(true);
+
+        BlackBoxInt blackBoxInt = blackBoxIntConstructor.newInstance(0);
+        String input;
+        while (!END_COMMAND.equals(input = SCANNER.nextLine())){
+            final String[] tokens = input.split("_");
+            final String methodName = tokens[0];
+            final Integer value = Integer.parseInt(tokens[1]);
+
+            final Method currentMethod = NAME_METHOD_MAP.get(methodName);
+            currentMethod.invoke(blackBoxInt,value);
+            final Field result = blackBoxInt.getClass().getDeclaredField(RESULT);
+            result.setAccessible(true);
+            System.out.println(result.get(blackBoxInt));
+
+        }
+        SCANNER.close();
 
     }
 }
